24db01f2a2a1994b13ee6de0c6bd9a7709bb92ba
Aasthasingh09-dotcom <aasthasinghrajput09@gmail.com>
2025-06-05 22:21:41 +0530

    Add files via upload
    assignment 2 task 1 and task 2 done successfullyy!

---
+++ task1.py
@@ -0,0 +1,6 @@
+a = input('enter an interger')
+a = int(a)
+if a % 2 == 0:
+    print(a ,'is an even integer')
+elif a % 2 != 0:
+    print(a , "is an odd integer")
---
+++ task2.py
@@ -0,0 +1,4 @@
+s=0
+for i in range(1,51):
+    s = s+i
+print('the sum of numers from 1 to 50 :',s)
